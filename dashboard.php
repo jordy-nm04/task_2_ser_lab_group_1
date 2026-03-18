70e3bc60496e305cc54ef674f36abb290dbbef10
Suwilanji Chapewa <suwilanjichapewa@gmail.com>
2026-03-18 09:48:45 +0200

    update

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn= new mysqli("localhost","root", "", "todolist");
+$conn = new mysqli("localhost","root","", "todolist",3307);
 if ($conn-> connect_error) {
     die("Connection Failed". $conn->connect_error);
 }
@@ -20,11 +20,6 @@ if (isset($_GET["complete"])){
     $conn->query("UPDATE tasks SET status = 'completed' WHERE id= '$id'");
     header("Location: dashboard.php");
 }
-if (isset($_GET["undo"])){
-    $id = $_GET["undo"];
-    $conn->query("UPDATE tasks SET status = 'pending' WHERE id='$id'");
-    header("Location: dashboard.php");
-}
 
 $result= $conn-> query("SELECT * FROM tasks ORDER BY id DESC");
 ?>
@@ -38,11 +33,12 @@ $result= $conn-> query("SELECT * FROM tasks ORDER BY id DESC");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Colored List</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
     <div class="container">
-       <div class="main-title"> <h1>Group 1’s Todo List</h1></div>
+       <div class="main-title"> <h1>Jordy’s Todo List</h1></div>
+       <?php include "task_counter.php"; ?>
+       <?php include "icon.php"; ?>
         <form action="dashboard.php" method="post">
             <input type="text" name="task" placeholder="Enter new task" id="">
             <button type="submit" name="addtask">Add Task</button>
@@ -52,19 +48,9 @@ $result= $conn-> query("SELECT * FROM tasks ORDER BY id DESC");
                 <li class="<?php echo $row["status"];?>">
                     <strong><?php echo $row["task"]; ?></strong>
                     <div class="actions">
-                        <?php if ($row['status'] !== 'completed'): ?>
-                             <a href="dashboard.php?complete=<?php echo $row['id']; ?>" class="complete-btn">
-                                 <i class="fa-solid fa-check"></i>
-                            </a>
-                        <?php else: ?>
-                            <a href="dashboard.php?undo=<?php echo $row['id']; ?>" class="undo-btn">
-                                <i class="fa-solid fa-rotate-left"></i>
-                            </a>
-                        <?php endif; ?>
+                        <a href="dashboard.php?complete=<?php echo $row['id'];?>">Complete</a>
 
-                        <a href="dashboard.php?delete=<?php echo $row['id']; ?>" class="delete-btn">
-                            <i class="fa-solid fa-trash"></i>
-                        </a>
+                        <a href="dashboard.php?delete=<?php echo $row['id'];?>">Delete</a>
                     </div>
                 </li>
             <?php endwhile?>
---
+++ icon.php
@@ -0,0 +1,48 @@
+<?php
+
+$conn= new mysqli("localhost","root", "", "todolist");
+if ($conn-> connect_error) {
+    die("Connection Failed". $conn->connect_error);
+}
+if (isset($_GET["delete"])){
+    $id= $_GET["delete"];
+    $conn->query("DELETE FROM tasks WHERE id='$id'");
+    header("Location: dashboard.php");
+}
+if (isset($_GET["complete"])){
+    $id= $_GET["complete"];
+    $conn->query("UPDATE tasks SET status = 'completed' WHERE id= '$id'");
+    header("Location: dashboard.php");
+}
+if (isset($_GET["undo"])){
+    $id = $_GET["undo"];
+    $conn->query("UPDATE tasks SET status = 'pending' WHERE id='$id'");
+    header("Location: dashboard.php");
+}
+?>
+
+<link rel = "stylesheet" href="style3.css">
+
+    <ul>
+            <?php while ($row= $result->fetch_assoc()):?>
+                <li class="<?php echo $row["status"];?>">
+                    <strong><?php echo $row["task"]; ?></strong>
+                    <div class="actions">
+                        <?php if ($row['status'] !== 'completed'): ?>
+                             <a href="dashboard.php?complete=<?php echo $row['id']; ?>" class="complete-btn">
+                                 <i class="fa-solid fa-check"></i>
+                            </a>
+                        <?php else: ?>
+                            <a href="dashboard.php?undo=<?php echo $row['id']; ?>" class="undo-btn">
+                                <i class="fa-solid fa-rotate-left"></i>
+                            </a>
+                        <?php endif; ?>
+
+                        <a href="dashboard.php?delete=<?php echo $row['id']; ?>" class="delete-btn">
+                            <i class="fa-solid fa-trash"></i>
+                        </a>
+                    </div>
+                </li>
+            <?php endwhile?>
+    </ul>
+
