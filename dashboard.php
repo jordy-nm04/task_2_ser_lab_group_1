cc28227b7d1aae26629c9f904b41aad4107578c8
Suwilanji Chapewa <suwilanjichapewa@gmail.com>
2026-03-18 09:58:40 +0200

    update

--- dashboard.php
+++ dashboard.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = new mysqli("localhost","root","", "todolist",3306);
+$conn = new mysqli("localhost","root","","todolist",3306);
 if ($conn-> connect_error) {
     die("Connection Failed". $conn->connect_error);
 }
